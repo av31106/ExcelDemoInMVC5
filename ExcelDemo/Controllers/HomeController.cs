6b80e98f44ac11bfff07159b8bde3c51980666d9
av31106 <av31106@users.noreply.github.com>
2020-09-22 20:03:20 +0530

    initial file

---
+++ ExcelDemo/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OfficeOpenXml;
+using System.IO;
+
+namespace ExcelDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetExeclFile()
+        {
+            ExcelPackage Ep = new ExcelPackage();
+            ExcelWorksheet Sheet = Ep.Workbook.Worksheets.Add("Report");
+            Sheet.Cells["A1"].Value = "Name";
+            Sheet.Cells["B1"].Value = "Department";
+            Sheet.Cells["C1"].Value = "Address";
+            Sheet.Cells["D1"].Value = "City";
+            Sheet.Cells["E1"].Value = "Country";
+            Sheet.Cells["A:AZ"].AutoFitColumns();
+
+            //Response.Clear();
+            //Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+            //Response.AddHeader("content-disposition", "attachment: filename=" + "Report.xlsx");
+            //Response.BinaryWrite(Ep.GetAsByteArray());
+            //Response.End();
+
+            return File(Ep.GetAsByteArray(), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "File.xlsx");
+        }
+    }
+}
